a14723135d4bd059b9e44b36bfd2266b437bf559
rybczany <piotr.maciej.rybak@gmail.com>
2017-08-28 23:21:15 +0200

    latest events

--- js/golden.js
+++ js/golden.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
 
 	$('.hide').hide();
+	$('.text-hide').hide();
 	$('.btn--circle').on('click', function(){
 		$('.hide').slideToggle();
 		$(this).toggleClass('active');
 		$('.btn--circle h3').toggleClass('active');
+		$('.btn--circle h3').toggle();
 	});
-
 
 })
